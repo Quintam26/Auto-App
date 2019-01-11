b4b503c6f3c2cc36eff9e1711d0c6bd1a79aff7f
Mario Quintana <pyrros2000@gmail.com>
2019-01-10 18:47:10 -0800

    Created logic for search bar and query appears in console log and in URL

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,7 +5,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h2>About</h2>
+        <p>About</p>
       </div>
     );
   }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import qs from 'query-string';
-// import PropTypes from 'prop-types';
+import qs from 'query-string';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
 
@@ -8,12 +8,44 @@ class Search extends Component {
     search: ''
   };
 
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    const { location } = this.props;
+    const { search = '' } = qs.parse(location.search);
+    
+    this.setState({ search });
+  }
+
+  handleChangeSearch = ({ target }) => {
+    this.setState({ search: target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { search } = this.state;
+    if(!search) return;
+
+    const { history } = this.props;
+    history.push({
+      pathname: '/search',
+      search: qs.stringify({ search, page: 1 })
+    });
+    this.setState({ search: '' });
+  };
+
   render() {
+    const { search } = this.state;
+    console.log(search);
+
     return (
-      <form className="search-form">
+      <form className="search-form" onSubmit={event => this.handleSubmit(event)}>
         <label>
           Search for:&nbsp;
-          <input/>
+          <input name="name" value={search} onChange={this.handleChangeSearch}/>
         </label>
         <button>Search</button>
       </form>
